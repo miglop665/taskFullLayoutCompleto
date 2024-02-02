593f88e8fd11e5ec94de3879f0bf57e5093c775a
miglop665 <miglop665@alu.centroafuera.es>
2024-02-02 13:37:19 +0100

    texto añadido en los Mains

--- app/src/main/java/com/example/taskfulllayout/MainActivity3.kt
+++ app/src/main/java/com/example/taskfulllayout/MainActivity3.kt
@@ -6,15 +6,18 @@ import android.os.Bundle
 import android.widget.ImageButton
 
 class MainActivity3 : AppCompatActivity() {
+    //Declaramos una variable de tipo Imagebutton
     private lateinit var volver: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //asignamos el MainActivity3.kt al activity_main3.xml correspondiente
         setContentView(R.layout.activity_main3)
 
+        //guardamos la referencia a los objetos del activity_main2.xml en la variable previamente declarada
         volver=findViewById(R.id.botonVolver)
 
-
+        //Al pulsar el icono de volver nos manda a la MainActivity
         volver.setOnClickListener {
             val intent = Intent(this@MainActivity3,MainActivity::class.java)
             startActivity(intent)
